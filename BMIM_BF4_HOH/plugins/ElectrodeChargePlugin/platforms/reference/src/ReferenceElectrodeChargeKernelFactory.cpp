3563b9787571773b744b24e1be58b9409126855e
AndyHu <andyyuan.hu@gmail.com>
2025-10-27 19:42:50 +0800

    灌完真·OpenMM了

--- BMIM_BF4_HOH/plugins/ElectrodeChargePlugin/platforms/reference/src/ReferenceElectrodeChargeKernelFactory.cpp
+++
@@ -1,9 +0,0 @@
-#include "ReferenceElectrodeChargeKernelFactory.h"
-#include "ReferenceElectrodeChargeKernel.h"
-
-using namespace ElectrodeChargePlugin;
-using namespace OpenMM;
-
-KernelImpl* ReferenceElectrodeChargeKernelFactory::createKernelImpl(std::string name, const Platform& platform, ContextImpl& context) const {
-    return new ReferenceCalcElectrodeChargeKernel(name, platform);
-}
